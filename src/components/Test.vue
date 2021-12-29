65f023b870f039a23eb22d4c2b5ad5d624abe4bd
Oliver Inglis <ollie@theinglistwins.com>
2021-12-29 15:02:46 +0000

    no working

---
+++ src/components/Test.vue
@@ -0,0 +1,18 @@
+<template>
+    <div :style="calculatedBackgroundColor" class="task-tracker-wrap">
+
+  </div>
+</template>
+
+<script  lang="ts">
+import {defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Work",
+
+});
+</script>
+
+<style>
+
+</style>
