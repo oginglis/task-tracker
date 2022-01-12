da3e037667fa94af061f489b8b18450c90040ca6
Oliver Inglis <ollie@theinglistwins.com>
2022-01-12 16:24:20 +0000

    work on lists

---
+++ src/components/Schedule.vue
@@ -0,0 +1,25 @@
+<template>
+<div></div>
+</template>
+
+<script lang="ts">
+
+import { defineComponent} from "vue";
+import * as dayjs from 'dayjs';
+
+export default defineComponent({
+  name: "TodoList",
+  created(){
+      dayjs()
+  },
+  data: function() {
+      return {
+          thisWeek: null,
+      }
+  }
+});
+</script>
+
+<style scoped>
+
+</style>
